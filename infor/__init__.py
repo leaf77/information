4f14fae719d62c928ce507352f0244827b925a56
leaf77 <1104890047@qq.com>
2019-05-28 16:01:25 +0800

    抽取创建到函数中&配置名字作为参数

--- infor/__init__.py
+++ infor/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+#可以用来指定session的保存位置
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import CSRFProtect
 from redis import StrictRedis
@@ -6,15 +7,21 @@ from sqlalchemy.orm import Session
 
 from config import config
 
-app = Flask(__name__)
-"""加载配置 """
-app.config.from_object(config["development"])
+# 初始化数据库
+# 在flask很多扩展里面都可以先初始化扩展的对象，然后再去调用init_app方法去初始化
+db = SQLAlchemy()
 
-#初始化数据库
-db=SQLAlchemy(app)
-#初始化redis存储对象
-redis_store=StrictRedis(host=config["development"].REDIS_HOST,port=config["development"].REDIS_PORT)
-#开启当前项目CSRF保护,只做服务器验证功能
-CSRFProtect(app)
-#设置session保存指定位置
-Session(app)
+def create_app(config_name):
+    app = Flask(__name__)
+    #加载配置
+    app.config.from_object(config[config_name])
+    #通过app初始化
+    db.init_app(app)
+    #初始化redis存储对象
+    redis_store=StrictRedis(host=config[config_name].REDIS_HOST,port=config[config_name].REDIS_PORT)
+    #开启当前项目CSRF保护,只做服务器验证功能
+    CSRFProtect(app)
+    #设置session保存指定位置
+    Session(app)
+
+    return app
--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@ from flask import session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from infor import app,db
+from infor import create_app,db
 
+#通过指定的配置名字创建对应配置的app
+#create_app 就类似于工厂方法
+app=create_app('development')
 
 manager = Manager(app)
 #将app于db关联
