0b183886e8867749e29637e1acfb61f42c6a1101
leaf77 <1104890047@qq.com>
2019-05-28 15:09:22 +0800

    抽取项目配置到单独的文件

---
+++ config.py
@@ -0,0 +1,26 @@
+from redis import StrictRedis
+
+class Config(object):
+    """项目的配置信息"""
+    DEBUG = True
+
+    SECRET_KEY="isfTBhp56eIDqQbyhuZ8VE12zZZwnH8L8TMtPiyi+XDTRl6H3DyEAzxiTZZ9y3mu"
+
+    """为数据库添加配置"""
+    SQLALCHEMY_DATABASE_URL="mysql://root:mysql@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    """Redis的配置"""
+    REDIS_HOST="127.0.0.1"
+    REDIS_PORT=6379
+
+    """Session保存配置"""
+    SESSION_TYPE="redis"
+    """开启Session签名"""
+    SESSION_USE_SIGNER=True
+    """指定Session保存的redis"""
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    """设置需要过期"""
+    SESSION_PERMANENT=False
+    """设置过期时间（两天）"""
+    PERMANENT_SESSION_LIFETIME = 86400*2
