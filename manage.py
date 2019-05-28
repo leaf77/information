55a7f7eab92ebb6516707e090f6e1ab308c3037b
leaf77 <1104890047@qq.com>
2019-05-28 10:47:29 +0800

    项目配置创建&数据库配置

--- manage.py
+++ manage.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+class Config(object):
+
+    """项目的配置信息"""
+    DEBUG = True
+    """为数据库添加配置"""
+    SQLALCHEMY_DATABASE_URL="mysql://root:mysql@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+
+ """加载配置 """
+app.config.from_object(Config)
+
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -9,4 +23,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=9999)
+
