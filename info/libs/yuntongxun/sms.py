d0f5c9bfe0ac4d14b363571a7966d63a64277de8
leaf77 <1104890047@qq.com>
2019-05-29 15:17:13 +0800

    发送短信验证码到后端逻辑实现

--- info/libs/yuntongxun/sms.py
+++ info/libs/yuntongxun/sms.py
@@ -60,6 +60,7 @@ class CCP(object):
         # @param datas 内容数据 格式为数组 例如：{'12','34'}，如不需替换请填 ''
         # @param temp_id 模板Id
         result = self.rest.sendTemplateSMS(to, datas, temp_id)
+        print(result)
 
         # 如果云通讯发送短信成功，返回的字典数据result中statuCode字段的值为"000000"
         if result.get("statusCode") == "000000":
@@ -73,4 +74,4 @@ class CCP(object):
 if __name__ == '__main__':
     ccp = CCP()
     # 注意： 测试的短信模板编号为1
-    ccp.send_template_sms('18586870956', ['666666', 5], 1)
+    ccp.send_template_sms('13041019597', ['666666', 5], 1)
--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,11 +1,83 @@
-from flask import abort
+import random
+import re
+
+from flask import abort, jsonify
 from flask import current_app
 from flask import make_response
 from flask import request
 
 from info import constants, redis_store
+from info.libs.yuntongxun.sms import CCP
+from info.utils.response_code import RET
 from . import passport_blu
 from info.utils.captcha.captcha import captcha
+
+@passport_blu.route('/sms_code', methods=['POST'])
+def send_sms_code():
+    """
+    发送短信的逻辑
+    1.获取参数：手机号，图片验证码内容，图片验证码的编号（随机值）
+    2.校验参数（参数是否符合规则，判断是否有值）
+    3.先从redis中取出真实的验证码内容
+    4.与用户的验证码内容进行对比，如果对比不一致，那么返回验证码输入错误
+    5。如果一致，生成验证码的内容（随机数据）
+    6.发送短信验证码
+    7.告知发送结果
+    :return:
+    """
+
+    '{"mobile":"18811111111","image_code":"AAAA"}'
+    #1.获取参数：手机号，图片验证码内容，图片验证码的编号（随机值）
+    # params_dict=json.loads(request.data)
+    params_dict = request.json
+
+    mobile = params_dict.get("mobile")
+    image_code = params_dict.get("image_code")
+    image_code_id = params_dict.get("image_code_id")
+
+    # 2.校验参数（参数是否符合规则，判断是否有值）
+    #判断参数是否有值
+    if not all([mobile, image_code, image_code_id]):
+        # {"error":"4100","errmsg":"参数有误"}
+        return jsonify(errno = RET.PARAMERR,errmsg="参数有误")
+    #校验手机号是否正确
+    if not re.match('1[35678]\\d{9}',mobile):
+        return jsonify(errno=RET.PARAMERR,errmsg="手机号格式不正确")
+
+    # 3.先从redis中取出真实的验证码内容
+    try:
+        real_image_code = redis_store.get("ImageCodeId_"+image_code_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据查询失败")
+
+    if not real_image_code:
+        return jsonify(errno=RET.NODATA,errmsg="图片验证码已过期")
+
+    #4. 与用户的验证码内容进行对比，如果对比不一致，那么返回验证码输入错误
+    if real_image_code.upper()!=image_code.upper():
+        return jsonify(errno=RET.DATAERR,errmsg="验证码输入错误")
+
+    #5.如果一致，生成短信验证码的内容（随机数据）
+    #随机数字，保证数字长度为6位，不够在前面补上0
+    sms_code_str = "%06d"%random.randint(0,999999)
+    current_app.logger.debug("短信验证码内容是：%s" % sms_code_str)
+
+    #6.发送短信验证码
+    result = CCP().send_template_sms(mobile, [sms_code_str,constants.SMS_CODE_REDIS_EXPIRES/5],"1")
+    if result != 0:
+        #代表发送不成功
+        return jsonify(errno=RET.THIRDERR,errmsg="发送短信失败")
+
+    #保存验证码内容到redis
+    try:
+        redis_store.set("SMS_"+mobile, sms_code_str, constants.SMS_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据保存失败")
+
+    #7. 告知发送结果
+    return jsonify(errno=RET.OK,errmsg="发送成功")
 
 @passport_blu.route("/image_code")
 def get_image_code():
@@ -37,5 +109,5 @@ def get_image_code():
     # 5.返回验证码图片
     response = make_response(image)
     # 设置数据的类型，一边浏览器更加智能识别其是什么类型
-    response.hearders["Content-Type"]="image/jpg"
+    response.headers["Content-Type"]="image/jpg"
     return response
