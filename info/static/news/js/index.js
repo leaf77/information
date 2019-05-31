2f752d96dabd28c4563897022647278376ac4069
leaf77 <1104890047@qq.com>
2019-05-31 19:04:04 +0800

    首页新闻数据展示

--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,10 +1,14 @@
-var currentCid = 0; // 当前分类 id
+var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
 var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+    //界面加载完成之后去加载新闻数据
+    updateNewsData()
+
+
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -46,5 +50,35 @@ $(function () {
 })
 
 function updateNewsData() {
-    // TODO 更新新闻数据
+    //更新新闻数据
+    var params = {
+        "cid":currentCid,
+        "page":cur_page
+    }
+    $.get("/news_list",params,function (resp) {
+        if(resp.errno == "0"){
+            //代表请求成功
+            //清除已有数据
+            $(".list_con").html("")
+            //添加请求成功之后返回的数据
+
+            //显示数据
+            for (var i=0;i<resp.data.news_List_li.length;i++) {
+                var news = resp.data.news_List_li[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+        }else{
+            //请求失败
+            alert(resp.errmsg)
+        }
+    })
 }
