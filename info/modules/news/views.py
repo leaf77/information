e597cedea1502d5de0576d8655e658c475805289
leaf77 <1104890047@qq.com>
2019-06-01 15:16:44 +0800

    首页和详情页的基类模板抽取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -9,5 +9,8 @@ def news_detail(news_id):
     :param news_id:
     :return:
     """
+    data = {
 
-    return render_template("news/detail.html")
+    }
+
+    return render_template("news/detail.html",data = data)
