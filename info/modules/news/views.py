6bd5acef9c458ebbe8e78c4be9ae758097f03fb6
leaf77 <1104890047@qq.com>
2019-06-02 09:21:15 +0800

    新闻详情页数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,3 +1,4 @@
+from flask import abort
 from flask import current_app
 from flask import session
 
@@ -33,9 +34,25 @@ def news_detail(news_id):
     for news in news_list:
         news_dict_li.append(news.to_basic_dict())
 
+    #查询新闻数据
+    news = None
+
+    try:
+        News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    if not news:
+        #报404错误，404错误统一显示页面后续再处理
+        abort(404)
+
+    #更新新闻的点击次数
+    news.clicks +=1
+
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
+        "news":news.to_dict()
     }
 
     return render_template("news/detail.html",data = data)
