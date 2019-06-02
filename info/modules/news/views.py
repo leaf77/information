1501a6b0fd8409f82544f6b13370edb59d855383
leaf77 <1104890047@qq.com>
2019-06-02 09:35:21 +0800

    显示是否收藏的逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -49,10 +49,13 @@ def news_detail(news_id):
     #更新新闻的点击次数
     news.clicks +=1
 
+    is_collected = True
+
     data = {
         "user": user.to_dict() if user else None,
         "news_dict_li": news_dict_li,
-        "news":news.to_dict()
+        "news":news.to_dict(),
+        "is_collected":is_collected
     }
 
     return render_template("news/detail.html",data = data)
